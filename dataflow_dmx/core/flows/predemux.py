afc2fbf15f2ecd57ae3297a61f5554d206568d9d
glrs <5999366+glrs@users.noreply.github.com>
2025-07-04 17:38:38 +0200

    refactor: replace prefect decorators with step decorators for unbiased example

--- dataflow_dmx/core/flows/predemux.py
+++ dataflow_dmx/core/flows/predemux.py
@@ -1,15 +1,19 @@
+import logging
 import subprocess
 from pathlib import Path
 
-from prefect import flow, get_run_logger, task
+from dataflow_dmx.core.adapters.adapter_list import ADAPTERS  # type: ignore
+from dataflow_dmx.core.adapters.base import DemuxConfig  # type: ignore
+from dataflow_dmx.core.utils.step import (
+    step,  # For demonstration purposes to imitate (a tracking decorator or Prefect, etc. steps, whatever we decide to use)
+)
 
-from core.adapters.adapter_list import ADAPTERS  # type: ignore
-from core.adapters.base import DemuxConfig  # type: ignore
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
-@task(retries=2)
+@step  # Example of automatically using the function's name as the step name
 def build_config(run_path: Path, instrument: str) -> DemuxConfig:
-    logger = get_run_logger()
     adapter_cls = ADAPTERS[instrument.lower()]
     adapter = adapter_cls(run_path)
     cfg = adapter.build_demux_config()
@@ -17,9 +21,9 @@ def build_config(run_path: Path, instrument: str) -> DemuxConfig:
     return cfg
 
 
-@task
+@step()  # Also an example of auto-naming a step
 def run_demux(cfg: DemuxConfig):
-    logger = get_run_logger()
+    # logger = get_run_logger()
     if not cfg.cmd:  # e.g. PromethION
         logger.info("No demux needed for %s", cfg.run_id)
         return
@@ -27,12 +31,20 @@ def run_demux(cfg: DemuxConfig):
     # subprocess.run(cfg.cmd, check=True)  # Bypassed since bcl-convert is not installed
 
 
-@task
+@step(
+    "do_something"
+)  # Example of explicitly overriding a step's name (positional argument)
 def another_step(cfg: DemuxConfig):
     pass
 
 
-@flow(name="production_demux_flow")
+@step(
+    name="do_soomething_else"
+)  # Example of explicitly overriding a step's name (keyword argument)
+def yet_another_step(cfg: DemuxConfig):
+    pass
+
+
 async def production_demux_flow(run_path: str, instrument: str):
     """
     Parameters
@@ -43,4 +55,5 @@ async def production_demux_flow(run_path: str, instrument: str):
     config = build_config(Path(run_path), instrument)
     run_demux(config)
     another_step(config)
+    yet_another_step(config)
     return True
